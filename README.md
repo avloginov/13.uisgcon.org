3e9213a3030e714a481a384f216c4f9308834960
Roman Vabishchevych <roma@se7ensky.com>
2016-09-07 23:08:36 +0300

    deploy

---
+++ README.md
@@ -0,0 +1,11 @@
+```npm install``` — install packages
+
+```verstat serve``` — for development, http://localhost:8080
+
+or
+
+```verstat generate``` — generate static html+css+js to server/public
+
+–––
+
+```./deploy``` — generate static and push2deploy to dokku, and push to github
--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,11 @@
 npm install ; verstat -e static generate
 cd server
 git init
-git add -A
-git commit -m 'auto deploy2dokku'
+git add .
+git commit -m 'auto deploy'
 git remote add dokku dokku@app.se7ensky.com:uisgcon
+git remote add github git@github.com:sapran/12.uisgcon.org.git
 git push -f dokku master
+git push -f github out
 rm -rf .git
 cd ..
